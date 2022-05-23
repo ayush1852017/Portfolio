4af0e4b29574584a320e11a0bf1193cc48a4065a
Ayush Bajpai <46336517+ayush1852017@users.noreply.github.com>
2022-05-23 23:24:23 +0530

    Update Techstack.jsx

--- Vite-Portfolio/src/components/About/Techstack.jsx
+++ Vite-Portfolio/src/components/About/Techstack.jsx
@@ -18,9 +18,6 @@ import {
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <CgCPlusPlus />
-      </Col> */}
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
       </Col>
@@ -33,21 +30,6 @@ function Techstack() {
       <Col xs={4} md={2} className="tech-icons">
         <DiMongodb />
       </Col>
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <SiNextdotjs />
-      </Col> */}
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col> */}
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <SiFirebase />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPytorch />
-      </Col> */}
     </Row>
   );
 }
